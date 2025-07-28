94fb38b55af596db342ef0c77b3b185911724a47
Maria Clara <mariaclara.dg25@gmail.com>
2025-07-28 09:35:15 -0300

    Initial Commit

---
+++ SpotifyExplorer/SpotifyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyExplorerApp.swift
+//  SpotifyExplorer
+//
+//  Created by Maria Clara Dias on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
