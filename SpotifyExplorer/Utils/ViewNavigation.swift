3e8d6e0b322915c44f25e38391bf3d627a345201
Maria Clara <mariaclara.dg25@gmail.com>
2025-09-18 08:29:42 -0300

    adjustment in the BackButton Environment

--- SpotifyExplorer/Utils/ViewNavigation.swift
+++ SpotifyExplorer/Utils/ViewNavigation.swift
@@ -21,11 +21,11 @@ extension View {
 }
 
 private struct BackButton: View {
-    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         Button(action: {
-            presentationMode.wrappedValue.dismiss()
+            dismiss()
         }) {
             Image(systemName: "chevron.left")
                 .foregroundColor(.white)
